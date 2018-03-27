a263621fcf1b8856c381d137b36f884eea58ad55
kkrepak <32241042+kkrepak@users.noreply.github.com>
2018-03-27 11:35:53 +0300

    lab2

--- Program.cs
+++ Program.cs
@@ -1,118 +1,243 @@
 ﻿using System;
 using System.IO;
+using System.Collections;
 using System.Text;
 
-namespace lab1
+namespace lab2
 {
-		class Program
+	class Program
+	{
+		static char ConvertToBase64 (string str)
 		{
-		static public char[] alphabet = new char[] { 'А', 'Б', 'В', 'Г', 'Ґ', 'Д', 'Е', 'Є', 'Ж', 'З', 'И', 'І', 'Ї', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'ґ', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ю', 'ь', 'я', '.', ',', ' ', '\n', '«', '»', '!', '(', ')', '[', ']', '?', '\'' };
-
-		static int LettersInText(string path)
-		{
-			int res = 0;
-			using (StreamReader sr = new StreamReader(path))
+			switch (str)
 			{
-				while (sr.Read() != -1) res++;
+				case "000000": return 'A';
+				case "000001": return 'B';
+				case "000010": return 'C';
+				case "000011": return 'D';
+				case "000100": return 'E';
+				case "000101": return 'F';
+				case "000110": return 'G';
+				case "000111": return 'H';
+				case "001000": return 'I';
+				case "001001": return 'J';
+				case "001010": return 'K';
+				case "001011": return 'L';
+				case "001100": return 'M';
+				case "001101": return 'N';
+				case "001110": return 'O';
+				case "001111": return 'P';
+				case "010000": return 'Q';
+				case "010001": return 'R';
+				case "010010": return 'S';
+				case "010011": return 'T';
+				case "010100": return 'U';
+				case "010101": return 'V';
+				case "010110": return 'W';
+				case "010111": return 'X';
+				case "011000": return 'Y';
+				case "011001": return 'Z';
+				case "011010": return 'a';
+				case "011011": return 'b';
+				case "011100": return 'c';
+				case "011101": return 'd';
+				case "011110": return 'e';
+				case "011111": return 'f';
+				case "100000": return 'g';
+				case "100001": return 'h';
+				case "100010": return 'i';
+				case "100011": return 'j';
+				case "100100": return 'k';
+				case "100101": return 'l';
+				case "100110": return 'n';
+				case "100111": return 'm';
+				case "101000": return 'o';
+				case "101001": return 'p';
+				case "101010": return 'q';
+				case "101011": return 'r';
+				case "101100": return 's';
+				case "101101": return 't';
+				case "101110": return 'u';
+				case "101111": return 'v';
+				case "110000": return 'w';
+				case "110001": return 'x';
+				case "110010": return 'y';
+				case "110011": return 'z';
+				case "110100": return '0';
+				case "110101": return '1';
+				case "110110": return '2';
+				case "110111": return '3';
+				case "111000": return '4';
+				case "111001": return '5';
+				case "111010": return '6';
+				case "111011": return '7';
+				case "111100": return '8';
+				case "111101": return '9';
+				case "111110": return '+';
+				case "111111": return '/';
+				default: return '=';
 			}
-			Console.WriteLine("number of letters " + res);
-			return res;
 		}
 
-		static int[] SomeLettersInText(string path)
+		public static BitArray Read(string filename)
 		{
-			int[] result = new int[alphabet.Length];
-			using (StreamReader sr = new StreamReader(path))
+			byte[] data;
+			using (StreamReader sr = new StreamReader(filename, Encoding.UTF8))
 			{
-				string text = sr.ReadToEnd();
-				for (int i = 0; i < alphabet.Length; i++)
+				string temp = sr.ReadToEnd();
+				Console.WriteLine(temp);
+				data = new byte[temp.Length];
+				data = System.Text.Encoding.UTF8.GetBytes(temp);
+			}
+			BitArray bitmap = new BitArray(data.Length * 8);
+			int i = 0;
+			for(int mybyte = 0; mybyte < data.Length; mybyte++)
+			{
+				if(data[mybyte] >= 128)
 				{
-					int startIndex = -1;
-					int hitCount = 0;
-					while (true)
+					bitmap[i] = true;
+					data[mybyte] -= 128;
+				}
+				else
+				{
+					bitmap[i] = false;
+				}
+
+				i++; 
+
+				if (data[mybyte] >= 64)
+				{
+					bitmap[i] = true;
+					data[mybyte] -= 64;
+				}
+				else
+				{
+					bitmap[i] = false;
+				}
+
+				i++;
+
+				if (data[mybyte] >= 32)
+				{
+					bitmap[i] = true;
+					data[mybyte] -= 32;
+				}
+				else
+				{
+					bitmap[i] = false;
+				}
+
+				i++;
+
+				if (data[mybyte] >= 16)
+				{
+					bitmap[i] = true;
+					data[mybyte] -= 16;
+				}
+				else
+				{
+					bitmap[i] = false;
+				}
+
+				i++;
+
+				if (data[mybyte] >= 8)
+				{
+					bitmap[i] = true;
+					data[mybyte] -= 8;
+				}
+				else
+				{
+					bitmap[i] = false;
+				}
+
+				i++;
+
+				if (data[mybyte] >= 4)
+				{
+					bitmap[i] = true;
+					data[mybyte] -= 4;
+				}
+				else
+				{
+					bitmap[i] = false;
+				}
+
+				i++;
+
+				if (data[mybyte] >= 2)
+				{
+					bitmap[i] = true;
+					data[mybyte] -= 2;
+				}
+				else
+				{
+					bitmap[i] = false;
+				}
+
+				i++;
+
+				if (data[mybyte] == 1)
+				{
+					bitmap[i] = true;
+				}
+				else
+				{
+					bitmap[i] = false;
+				}
+				i++;
+			}
+			return bitmap;
+		}
+
+		public static string Convert(BitArray barr)
+		{
+			string result = "", fin = "";
+			for (int i = 0; i < barr.Length; i += 6)
+			{
+				string temp = "";
+				try
+				{
+					for (int j = 0; j <6; j++)
 					{
-						startIndex = text.IndexOf(alphabet[i], startIndex + 1, text.Length - startIndex - 1);
-						if (startIndex < 0)
-							break;
-						hitCount++;
+						temp += (barr[i + j] ? "1" : "0");
 					}
-					result[i] = hitCount;
-					Console.WriteLine("for letter " + alphabet[i] + " -- " + hitCount);
+					result += ConvertToBase64(temp);
 				}
-			}
-			return result;
-		}
-
-		static double[] Frequency(int[] countEachLetter, int count)
-		{
-			double[] frequency = new double[alphabet.Length];
-			for(int i = 0; i < alphabet.Length; i++)
-			{
-				frequency[i] = (double)countEachLetter[i] / (double)count;
-			}
-			return frequency;
-		}
-
-		static double Entropia(double[] frequency, int count)
-		{
-			double result = 0, temp;
-			Console.WriteLine("freq");
-			for (int i = 0; i < alphabet.Length; i++)
-			{
-				temp = frequency[i] * Math.Log(frequency[i], 2);
-				if ( temp < 0)
+				catch
 				{
-					result += temp;
+					if (barr.Length % 6 == 2)
+					{
+						temp += "0000";
+						fin += "==";
+					}
+					else if (barr.Length % 6 == 4)
+					{
+						temp += "00";
+						fin += "=";
+					}
+
+					result += ConvertToBase64(temp);
 				}
 			}
-			return -result;
-		}
-
-		static double Data(double entropia, int count)
-		{
-			return entropia * count;
-		}
-
-		static void Check(string path)
-		{
-			//загальна кількість символів у тексті
-			int count = LettersInText(path + ".txt");
-			Console.WriteLine("Загальна кількість символів у тексті -   " + count);
-			Console.WriteLine("\n\n\n");
-
-			//кількість кожного символу у тексті
-			int[] countEachLetter = SomeLettersInText(path + ".txt");
-
-			//частота кожного символу у тексті
-			double[] frequency = Frequency(countEachLetter, count);
-			/*for (int i = 0; i < alphabet.Length; i++)
-			{
-				Console.WriteLine("Частота символу \'" + alphabet[i] + "\'  -  " + frequency[i]);
-			}*/
-
-			//загальна ентропія тексту
-			double entropia = Entropia(frequency, count);
-			Console.WriteLine("\n\nЗагальна ентропія тексту -   " + entropia);
-
-			//кількість інформації
-			double data = Data(entropia, count);
-			Console.WriteLine("\n\nКількість інформації -   " + data);
-
-			//порівняння розмірів файлу і кількості інформації
-			FileInfo fi = new FileInfo(path + ".txt");
-			Console.WriteLine("Кількість інформації: " + (data/1024.0) + "\nРозмір файла: " + (fi.Length) + "\nВідношення: " + (fi.Length * 1024.0 / data));
-			Console.WriteLine("7z - {0} ----- {1}", new FileInfo(path + ".7z").Length, data);
-			Console.WriteLine("tar - {0} ----- {1}", new FileInfo(path + ".tar").Length, data);
-			Console.WriteLine("bz2 - {0} ----- {1}", new FileInfo(path + ".txt.bz2").Length, data);
-			Console.WriteLine("gz - {0} ----- {1}", new FileInfo(path + ".txt.gz").Length, data);
-			Console.WriteLine("xz - {0} ----- {1}", new FileInfo(path + ".txt.xz").Length, data);
+			return result + fin;
 		}
 
 		static void Main(string[] args)
 		{
-			Check("test1");
-			Check("test2");
-			Check("test3");
+			BitArray barr = Read("test1.txt");
+			Console.Write(Convert(barr));
+			Console.WriteLine("\n\n");
+
+			barr = Read("test2.txt");
+			Console.Write(Convert(barr));
+			Console.WriteLine("\n\n");
+
+			barr = Read("test3.txt");
+			Console.Write(Convert(barr));
+			Console.WriteLine("\n\n");
+
+
 			Console.ReadKey();
 		}
 	}
